82f1d7d9e145562d96663f8ccc2ec45a38a78c0d
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2021-12-14 11:40:22 +0000

    デプロイサーバー起動準備

--- app/controllers/public/courses_controller.rb
+++ app/controllers/public/courses_controller.rb
@@ -1,2 +1,15 @@
 class Public::CoursesController < ApplicationController
+  def index
+    @total_courses = Course.all
+    @courses = Course.all.page(params[:page]).per(8)
+  end
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+
+  def courses_params
+    params.require(:course).permit(:name, :totalpayment, :is_booked, :rest_id)
+  end
 end
